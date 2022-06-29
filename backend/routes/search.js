39f78b074a371595fb1c3ebd63f353eed75a7e09
Jerry Xu <jerryxwang9@gmail.com>
2022-06-29 09:27:14 -0700

    returned parsed data

--- backend/routes/search.js
+++ backend/routes/search.js
@@ -9,6 +9,7 @@ let near_by_search_string =
   process.env.GOOGLE_PLACES_API_KEY;
 
 let frontend_input = "restaurants%20near%20UBC";
+// Note: text_search returns max of 20 results unless passing in next_page_token to a subsequent request
 let text_search_string =
   "https://maps.googleapis.com/maps/api/place/textsearch/json?query=" +
   frontend_input +
@@ -19,20 +20,21 @@ let text_search_string =
 
 // Get restaurants info by query
 router.get("/searchRestaurantsByQuery", async (req, res) => {
-  //   try {
-  //     const response = await axios.get(text_search_string);
-  //     if (response?.data?.results) {
-  //       const parsed_data = parseRestResult(response?.data?.results);
-  //       console.log(parsed_data);
-  //     }
-  //     res.json(response.data);
-  //   } catch (err) {
-  //     console.error(err);
-  //     res.json(err);
-  //   }
-  const parsed_data = parseRestResult(ex_text_search_result);
-  res.json(parsed_data);
-  // res.json(`"data": ${parsed_data}`);
+  try {
+    let frontend_input = "restaurants%20near%20UBC";
+    const response = await axios.get(text_search_string);
+    if (response?.data?.results) {
+      const parsed_data = parseRestResult(response?.data?.results);
+      console.log(parsed_data);
+      res.json(parsed_data);
+    } else {
+      console.log("No results from Google Places API");
+      res.json(response.data);
+    }
+  } catch (err) {
+    console.error(err);
+    res.json(err);
+  }
 });
 
 // TODO: Get restaurant info detail by restaurant id
--- backend/utils/parseRestResult.js
+++ backend/utils/parseRestResult.js
@@ -1,6 +1,25 @@
 const parseRestResult = (restaurants) => {
-  console.log(restaurants);
-  return restaurants;
+  let parsed_data = [];
+
+  restaurants.forEach((item) => {
+    let parsed_item = {
+      id: item?.place_id,
+      name: item?.name,
+      address: item?.formatted_address,
+      openNow: item?.opening_hours?.open_now,
+      businessStatus: item?.business_status,
+      GoogleRating: item?.rating,
+    };
+    parsed_data.push(parsed_item);
+  });
+  return parsed_data;
 };
+
+// `id: int,
+// name: string,
+// address: string,
+// openNow: bool,
+// businessStatus: string,
+// GoogleRating: string`;
 
 module.exports = parseRestResult;
