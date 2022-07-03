cf3f900793bd93cbe1ba6691acee9001776776e7
Jerry Xu <jerryxwang9@gmail.com>
2022-07-03 13:22:47 -0700

    changed to _id from place_id, also removed some comments

--- backend/routes/foodoList.js
+++ backend/routes/foodoList.js
@@ -8,7 +8,7 @@ const { FoodoListModel } = require("../models/FoodoList");
 const { UserAccountModel } = require("../models/UserAccount");
 
 // Create a new FoodoList
-// TODO
+// TODO!!!!!!!!!!!!!!
 router.post("/createFoodoList", async (req, res) => {
   try {
     // NOTE: We should probably change this to find an existing user account instead of creating a new one!!!
@@ -32,7 +32,7 @@ router.post("/createFoodoList", async (req, res) => {
 });
 
 // Get all the Foodo lists of a user
-// TODO
+// TODO!!!!!!!!!!!
 router.get("/getFoodoLists", async (req, res) => {});
 
 // Delete a Foodo list from db
@@ -74,30 +74,17 @@ router.patch("/addRestaurantToList", async (req, res) => {
 });
 
 // Delete a restaurant from a Foodo list
-// Modified from M4 (passed in place_id insteaad of restaurant id cuz it should be easier? like you dont need to find the restaurant id first)
 router.patch("/deleteRestaurantFromList", async (req, res) => {
   try {
-    // 1. try to find the restaurant by id using elemMatch (didn't work) (BECAUSE foodlistschema was using foodorestaurant schema?)
-    // const updatedList = await FoodoListModel.findByIdAndUpdate(
-    //   req.body.listID,
-    //   {
-    //     $pull: { restaurants: { $elemMatch: { _id: req.body.restaurantID } } },
-    //   }
-    // );
-    const updatedList = await FoodoListModel.findByIdAndUpdate(
-      req.body.listID,
-      {
-        $pull: {
-          restaurants: {
-            place_id: req.body.place_id,
-          },
+    const lastList = await FoodoListModel.findByIdAndUpdate(req.body.listID, {
+      $pull: {
+        restaurants: {
+          _id: req.body.restaurantID,
         },
-      }
-    );
-    console.log(updatedList);
-    res.json(updatedList);
+      },
+    });
+    res.json(lastList);
   } catch (err) {
-    console.log(err);
     res.json(err);
   }
 });
@@ -107,34 +94,12 @@ router.patch("/deleteRestaurantFromList", async (req, res) => {
 router.patch("/addNewUserToList", async (req, res) => {});
 
 // Set the "isValid" field of a Foodo Restaurant as isVisited
-// todo
 router.patch("/checkRestaurantOnList", async (req, res) => {
   try {
-    // const updatedList = await FoodoListModel.findByIdAndUpdate(
-    //   req.body.listID,
-    //   { $elemMatch: { place_id: req.body.place_id } },
-    //   {
-    //     $set: {
-    //       "restaurants.$.isVisited": req.body.isVisited,
-    //     },
-    //   }
-    // );
-
-    // const updatedList = await FoodoListModel.findOneAndUpdate(
-    //   { _id: req.body.listID, place_id: req.body.place_id },
-    //   null,
-    //   {
-    //     $set: {
-    //       "restaurants.$.isVisited": req.body.isVisited,
-    //     },
-    //   }
-    // );
-
-    // THIS WORKS!!
     const lastList = await FoodoListModel.findOneAndUpdate(
       {
         _id: req.body.listID,
-        restaurants: { $elemMatch: { place_id: req.body.place_id } },
+        restaurants: { $elemMatch: { _id: req.body.restaurantID } },
       },
       {
         $set: {
