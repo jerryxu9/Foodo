c221866de6f3fc58ee74fe5b549caaeb0fee3d6d
shirleyqi26 <69826368+shirleyqi26@users.noreply.github.com>
2022-07-07 18:12:04 -0700

    starting google auth

--- frontend/app/src/main/java/com/example/foodo/MainActivity.java
+++ frontend/app/src/main/java/com/example/foodo/MainActivity.java
@@ -11,6 +11,9 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.foodo.service.FoodoListService;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -32,12 +35,21 @@ public class MainActivity extends AppCompatActivity {
     private final String BASE_URL = "http://10.0.2.2:3000";
     private SearchView restaurantSearch;
     private Button mapButton;
-
+    private GoogleSignInClient mGoogleSignInClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Configure sign-in to request the user's ID, email address, and basic
+// profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        // Build a GoogleSignInClient with the options specified by gso.
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
 
         restaurantSearch = findViewById(R.id.restaurant_search);
         restaurantSearch.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
--- frontend/app/src/main/java/com/example/foodo/service/FoodoListService.java
+++ frontend/app/src/main/java/com/example/foodo/service/FoodoListService.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.PopupWindow;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +20,8 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.foodo.R;
 import com.example.foodo.objects.FoodoListCard;
 import com.example.foodo.objects.FoodoListCardAdapter;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import org.json.JSONArray;
@@ -71,7 +74,6 @@ public class FoodoListService {
         foodoLists.setAdapter(foodoListCardAdapter);
     }
 
-
     private void initializeFoodoLists() {
         String url = BASE_URL + "/getFoodoLists";
         HttpUrl httpUrl = HttpUrl.parse(url);
@@ -122,7 +124,6 @@ public class FoodoListService {
 
     }
 
-
     private void handleCreateFoodoListAction() {
         Log.d(TAG, "Pressed add Foodo restaurant button");
         LayoutInflater layoutInflater = (LayoutInflater) main_activity.getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
@@ -135,6 +136,7 @@ public class FoodoListService {
 
         container.findViewById(R.id.create_foodo_list_confirm_button).setOnClickListener((View v) -> {
             try {
+                authenticateAccount(GoogleSignIn.getLastSignedInAccount(main_activity));
                 createFoodoList(container);
             } catch (IOException e) {
                 e.printStackTrace();
@@ -154,8 +156,17 @@ public class FoodoListService {
         });
     }
 
-    private void createFoodoList(ViewGroup container) throws IOException {
+    private void authenticateAccount(GoogleSignInAccount account){
+        //user has never logged in
+        if(account == null){
 
+        }else{
+
+        }
+
+    }
+
+    private void createFoodoList(ViewGroup container) throws IOException {
         String url = BASE_URL + "/createFoodoList";
         HttpUrl httpUrl = HttpUrl.parse(url);
 
