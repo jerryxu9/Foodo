b1714fdc971dd3ca1e052124b4394ee772f15af3
Jerry Xu <jerryxwang9@gmail.com>
2022-07-14 16:20:31 -0700

    created a mock for axios for searchRestaurantInfoByID test

--- backend/tests/Restaurants/searchRestaurantInfoByID.test.js
+++ backend/tests/Restaurants/searchRestaurantInfoByID.test.js
@@ -1,9 +1,18 @@
+const { default: axios } = require("axios");
 const request = require("supertest");
 const app = require("../../server");
 const { ID_1_RESP } = require("./mock_response");
 
+jest.mock("axios");
+
 describe("/searchRestaurantInfoByID", () => {
   it("test with a valid query string", async () => {
+    axios.get.mockResolvedValue({
+      data: {
+        name: ID_1_RESP.name,
+      },
+    });
+
     const response = await request(app).get("/searchRestaurantInfoByID").query({
       id: "ChIJTYjW9soKhlQRwO6-7IRKVq0",
     });
