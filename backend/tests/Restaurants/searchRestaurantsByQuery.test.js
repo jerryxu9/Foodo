021d39a8edf4ec2df16f8fe18c0f4d1318b8dbdf
Jerry Xu <jerryxwang9@gmail.com>
2022-07-14 20:42:49 -0700

    added axios mock

--- backend/tests/Restaurants/searchRestaurantsByQuery.test.js
+++ backend/tests/Restaurants/searchRestaurantsByQuery.test.js
@@ -1,9 +1,16 @@
+const { default: axios } = require("axios");
 const request = require("supertest");
 const app = require("../../server");
 const { QUERY_1_RESP } = require("./mock_response");
 
+jest.mock("axios");
+
 describe("/searchRestaurantsByQuery", () => {
   it("test with a valid query string", async () => {
+    axios.get.mockResolvedValue({
+      data: QUERY_1_RESP,
+    });
+
     const response = await request(app).get("/searchRestaurantsByQuery").query({
       query: "chinese food in downtown vancouver",
       lat: 49.28272833333334,
@@ -15,6 +22,7 @@ describe("/searchRestaurantsByQuery", () => {
     );
 
     expect(response.body.length).toEqual(20);
+    expect(response.body).toEqual(QUERY_1_RESP);
     expect(response.statusCode).toBe(200);
   });
 });
