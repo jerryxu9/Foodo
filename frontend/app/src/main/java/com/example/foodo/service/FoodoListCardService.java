927a865d5b93ca81793d041010dfdd84bac68916
Ken Johnson <dragonkenkj@gmail.com>
2022-07-18 23:13:46 -0400

    Changed all IP from local to server

--- frontend/app/src/main/java/com/example/foodo/service/FoodoListCardService.java
+++ frontend/app/src/main/java/com/example/foodo/service/FoodoListCardService.java
@@ -37,7 +37,7 @@ public class FoodoListCardService {
     private final String TAG = "FoodoListCardService";
     private MediaType JSON = MediaType.parse("application/json; charset=utf-8");
     private String username, userID;
-    private final String BASE_URL = "http://10.0.2.2:3000";
+    private final String BASE_URL = "http://20.51.215.223:3000";
     private final OkHttpClient client = new OkHttpClient();
     private final AppCompatActivity foodoCardActivity;
     private final ArrayList<RestaurantCard> restaurantCardArrayList;
