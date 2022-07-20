e9ec56a1a3d26a5bc38fd42e4b6a81728c74fe62
Jerry Xu <jerryxwang9@gmail.com>
2022-07-19 18:46:55 -0700

    added .then for app

--- backend/index.js
+++ backend/index.js
@@ -4,11 +4,12 @@ const mongoose = require("mongoose");
 async function main() {
   mongoose
     .connect("mongodb://localhost:27017/cpen321")
-    .then(console.log("Successfully connected to the CPEN 321 database"));
-
-  app.listen(3000, (req, res) => {
-    console.log("Server successfully running at port 3000");
-  });
+    .then(console.log("Successfully connected to the CPEN 321 database"))
+    .then(() => {
+      app.listen(3000, (req, res) => {
+        console.log("Server successfully running at port 3000");
+      });
+    });
 }
 
 main().catch((err) => {
