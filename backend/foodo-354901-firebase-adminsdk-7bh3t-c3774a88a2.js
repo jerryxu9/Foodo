9ef063db9fecfaaa6d3eab7d5ac4a0d6e5f2b9dd
Ken Johnson <dragonkenkj@gmail.com>
2022-07-21 11:34:03 -0400

    Added firebase private key to env var

---
+++ backend/foodo-354901-firebase-adminsdk-7bh3t-c3774a88a2.js
@@ -0,0 +1,13 @@
+module.exports = {
+  type: "service_account",
+  project_id: "foodo-354901",
+  private_key_id: "c3774a88a203dc8ccda4a9361b922788b1fc6eb5",
+  private_key: process.env.FIREBASE_PRIVATE_KEY,
+  client_email: "firebase-adminsdk-7bh3t@foodo-354901.iam.gserviceaccount.com",
+  client_id: "115198289205947068058",
+  auth_uri: "https://accounts.google.com/o/oauth2/auth",
+  token_uri: "https://oauth2.googleapis.com/token",
+  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+  client_x509_cert_url:
+    "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-7bh3t%40foodo-354901.iam.gserviceaccount.com",
+};
--- backend/utils/firebase.js
+++ backend/utils/firebase.js
@@ -1,6 +1,6 @@
 var { initializeApp } = require("firebase-admin/app");
 var admin = require("firebase-admin");
-var serviceAccount = require("../foodo-354901-firebase-adminsdk-7bh3t-c3774a88a2.json");
+var serviceAccount = require("../foodo-354901-firebase-adminsdk-7bh3t-c3774a88a2");
 
 const app = initializeApp({
   credential: admin.credential.cert(serviceAccount),
