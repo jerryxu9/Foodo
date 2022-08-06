2e1b657ee5312310d6d6541034f0902b6de631a2
shirleyqi26 <69826368+shirleyqi26@users.noreply.github.com>
2022-08-05 18:06:49 -0700

    codacy

--- frontend/app/src/main/java/com/example/foodo/MainActivity.java
+++ frontend/app/src/main/java/com/example/foodo/MainActivity.java
@@ -18,7 +18,6 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.SearchView;
 import android.widget.TextView;
 import android.widget.Toast;
